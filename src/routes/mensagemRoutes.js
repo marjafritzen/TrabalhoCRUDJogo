4717c4f642c396a0eb837af6bb674aef39ed4af1
Vitória de Lázaro Floriani <129802507+Konanzinhaa@users.noreply.github.com>
2024-05-11 20:53:34 -0300

    Add files via upload

--- src/routes/mensagemRoutes.js
+++ src/routes/mensagemRoutes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const MensagemController = require('../controllers/MensagemController');
+const mensagemController = require('../controllers/MensagemController');
 
 // Rota para criar uma nova mensagem (CREATE)
-router.post('/', MensagemController.create);
+router.post('/', mensagemController.create);
 
 // Rota para listar todas as mensagens (READ)
-router.get('/', MensagemController.list);
+router.get('/', mensagemController.list);
 
 // Rota para mostrar uma mensagem específica (READ)
-router.get('/:id', MensagemController.show);
+router.get('/:id', mensagemController.show);
 
 // Rota para atualizar uma mensagem (UPDATE)
-router.put('/:id', MensagemController.update);
+router.put('/:id', mensagemController.update);
 
 // Rota para deletar uma mensagem (DELETE)
-router.delete('/:id', MensagemController.delete);
+router.delete('/:id', mensagemController.delete);
 
 module.exports = router;
